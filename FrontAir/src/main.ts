6f1df6a317da8c82d39ddeccc2ceed450db02717
Pepijn Robben <93707159+pepijngbr@users.noreply.github.com>
2024-05-21 18:52:18 +0200

    Installed vue-axios

--- FrontAir/src/main.ts
+++ FrontAir/src/main.ts
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 // import store from './store'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App).use(router).use(VueAxios, axios).use(createPinia()).mount('#app')
 // createApp(App).use(store).use(router).mount('#app')
