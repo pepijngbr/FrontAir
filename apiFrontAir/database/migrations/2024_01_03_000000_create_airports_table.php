8ba1ecd24a926a6d9b6149b6866fce2f2a3685f6
Pepijn Robben <93707159+pepijngbr@users.noreply.github.com>
2024-06-04 18:44:02 +0200

    Refactored Laravel migrations

--- apiFrontAir/database/migrations/2024_01_03_000000_create_airports_table.php
+++ apiFrontAir/database/migrations/2024_01_03_000000_create_airports_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,7 +17,6 @@ return new class extends Migration
             $table->string('country');
             $table->string('iata');
             $table->string('icao');
-            $table->string('timezone');
             $table->string('type')->default('medium_airport');
             $table->timestamps();
         });
